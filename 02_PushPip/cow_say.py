c005cd3192cbea709da25c52464525d0858912a5
Aleksei Romanov <raa_2001@mail.ru>
2024-02-21 23:31:34 +0300

    Add options support

---
+++ 02_PushPip/cow_say.py
@@ -0,0 +1,20 @@
+import argparse
+import cowsay
+import sys
+
+parser = argparse.ArgumentParser("cow_say")
+parser.add_argument("-e", type=str, dest="eye_string", default=cowsay.Option.eyes)
+parser.add_argument("-l", action="store_true")
+parser.add_argument("-f", type=str, dest="cowfile", default=None)
+parser.add_argument("-n", action="store_true")
+parser.add_argument("-T", type=str, dest="tongue_string", default=cowsay.Option.tongue)
+parser.add_argument("-W", type=int, dest="column", default=40)
+parser.add_argument("-b", action="store_true")
+parser.add_argument("-d", action="store_true")
+parser.add_argument("-g", action="store_true")
+parser.add_argument("-p", action="store_true")
+parser.add_argument("-s", action="store_true")
+parser.add_argument("-t", action="store_true")
+parser.add_argument("-w", action="store_true")
+parser.add_argument("-y", action="store_true")
+args = parser.parse_args()
