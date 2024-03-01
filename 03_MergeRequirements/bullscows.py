2ddaf7e5fde028cccea6eea59c9468e2951718f7
Aleksei Romanov <raa_2001@mail.ru>
2024-03-01 22:37:19 +0300

    Basic bullscows game is added
    Added module bullscows.py and an option to launch it in a reference-game mode.

---
+++ 03_MergeRequirements/bullscows.py
@@ -0,0 +1,54 @@
+import argparse
+import pathlib
+from typing import Tuple
+import urllib.request
+import random
+
+def bullscows(guess: str, secret: str) -> Tuple[int, int]:
+    bulls, cows = 0, 0
+    for guess_c, secret_c in zip(guess, secret):
+        if guess_c == secret_c:
+            bulls += 1
+        elif guess_c in secret:
+            cows += 1
+    return (bulls, cows)
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    secret = words[random.randint(0, len(words))]
+    bulls = 0
+    tries = 0
+    print(secret)
+    while bulls != len(secret):
+        guess = ask("Введите слово: ", words)
+        tries += 1
+        bulls, cows = bullscows(guess, secret)
+        inform("Быки: {}, Коровы: {}", bulls, cows)
+    print(tries)
+
+def _ask_impl(prompt: str, valid: list[str] = None) -> str:
+    valid_guess = False
+    while not valid_guess:
+        print(prompt, end="")
+        guess = input()
+        valid_guess = not valid or guess in valid
+    return guess
+
+def _inform_impl(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("bullscows")
+    parser.add_argument("dictionary", type=str)
+    parser.add_argument("length", type=int, nargs="?", default=5)
+    args = parser.parse_args()
+    dictionary_file = pathlib.Path(args.dictionary)
+    if dictionary_file.exists() and dictionary_file.is_file():
+        with open(args.dictionary, "r") as file:
+            words = [word.strip() for word in file.readlines()]
+    else:
+        # Try to download dictionary
+        with urllib.request.urlopen(args.dictionary) as file:
+            words = [word.decode().strip() for word in file.readlines()]
+    words = [word for word in filter(lambda word: len(word) == args.length, words)]
+    if len(words):
+        gameplay(_ask_impl, _inform_impl, words)
